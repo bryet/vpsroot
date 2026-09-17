0a09d36bafc6bb996045c034bb2a195ad658e16c
bryet <106591422+bryet@users.noreply.github.com>
2026-09-17 13:46:51 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -142,8 +142,8 @@ main(){
     install_base
     set_port
     set_passwd
-    sed -i "0,/^#\?Port/s/^#\?Port.*/Port ${sshport}/g" /etc/ssh/sshd_config
-    sed -i -E '0,/^[[:space:]]*#?[[:space:]]*Port[[:space:]]/s/^[[:space:]]*#?[[:space:]]*Port.*/Port $sshport/' /etc/ssh/sshd_config
+    #sed -i "0,/^#\?Port/s/^#\?Port.*/Port ${sshport}/g" /etc/ssh/sshd_config
+    sed -i -E '0,/^[[:space:]]*#?[[:space:]]*Port[[:space:]]/s/^[[:space:]]*#?[[:space:]]*Port.*/Port ${sshport/' /etc/ssh/sshd_config
     sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
     sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
     sed -i 's/^#\?RSAAuthentication.*/RSAAuthentication yes/g' /etc/ssh/sshd_config
